3d3b95a2bd71ba51a5f6b5a0da41821dc5fe4030
kimsoyol <naywinlhaing@gmail.com>
2024-01-07 16:21:09 +0630

    fix matcher

---
+++ components/enduser/painting/all-painting.tsx
@@ -0,0 +1,30 @@
+import { fetchAllPaintings } from "@/lib/data/paintings.data";
+import Image from "next/image";
+
+
+
+const AllPaintings = async() => {
+  const allPaintings = await fetchAllPaintings();
+
+  
+  return (
+    <div>
+      <h2>AllPainting</h2>
+        {allPaintings.map((painting, i) => {
+          return (
+            <div key={painting.id}>
+              <p>{painting.title}</p>
+              <Image 
+                src={`/${painting.imgURL}`}
+                width={500}
+                height={500}
+                alt='sup'
+              />
+              <p>{painting.author}</p>
+            </div>
+          )
+        })}
+    </div>
+  )
+}
+export default AllPaintings
