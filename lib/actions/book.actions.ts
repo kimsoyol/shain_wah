fa99cef0ed357710240e88d51bc849ef88bedfb4
kimsoyol <naywinlhaing@gmail.com>
2024-01-09 17:44:28 +0630

    build again :S

--- lib/actions/book.actions.ts
+++ lib/actions/book.actions.ts
@@ -1,87 +1,87 @@
-'use server'
+// 'use server'
 
-import { revalidatePath } from 'next/cache';
-import { redirect } from 'next/navigation';
-import {z } from 'zod'
-import path from "path";
-import { writeFile } from "fs/promises";
+// import { revalidatePath } from 'next/cache';
+// import { redirect } from 'next/navigation';
+// import {z } from 'zod'
+// import path from "path";
+// import { writeFile } from "fs/promises";
 
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
-
-
-type State = {
-  errors?: {
-    img?: string[];
-    title?: string[];
-    author?: string[];
-  };
-  message?: string | null;
-};
+// import { PrismaClient } from "@prisma/client";
+// const prisma = new PrismaClient();
 
 
-export async function createBook(prevState: State, formData: FormData) {
-  const schema = z.object({
-    img: z.any(),
-    title: z.string(),
-    author: z.string(),
-  });
+// type State = {
+//   errors?: {
+//     img?: string[];
+//     title?: string[];
+//     author?: string[];
+//   };
+//   message?: string | null;
+// };
 
-  const validatedData = schema.safeParse({
-    title: formData.get("title"),
-    author: formData.get("author"),
-    img: formData.get("img"),
-  });
 
-  if (!validatedData.success) {
-    return {
-      errors: validatedData.error.flatten().fieldErrors,
-      message: "Missing Fields.",
-    };
-  }
+// export async function createBook(prevState: State, formData: FormData) {
+//   const schema = z.object({
+//     img: z.any(),
+//     title: z.string(),
+//     author: z.string(),
+//   });
 
-  const { img, title, author } = validatedData.data;
+//   const validatedData = schema.safeParse({
+//     title: formData.get("title"),
+//     author: formData.get("author"),
+//     img: formData.get("img"),
+//   });
 
-  const buffer = Buffer.from(await img.arrayBuffer());
-  const filename = (
-    Date.now() +
-    "-" +
-    img.name.replaceAll(" ", "_")
-  ).toString();
+//   if (!validatedData.success) {
+//     return {
+//       errors: validatedData.error.flatten().fieldErrors,
+//       message: "Missing Fields.",
+//     };
+//   }
 
-  // check author first
-  const authorRecord = await prisma.author.findFirst({
-    where: {
-      name: author,
-    },
-  });
+//   const { img, title, author } = validatedData.data;
 
-  if (!authorRecord) {
-    throw new Error(`No author found with name ${validatedData.data.author}`);
-  }
+//   const buffer = Buffer.from(await img.arrayBuffer());
+//   const filename = (
+//     Date.now() +
+//     "-" +
+//     img.name.replaceAll(" ", "_")
+//   ).toString();
 
-  try {
-    await writeFile(path.join(process.cwd(), "/public/uploads/" + filename), buffer);
-    await prisma.book.create({
-      data: {
-        title: title,
-        author: {
-          connect: {
-            id: authorRecord.id,
-          },
-        },
-        //imgURL: `uploads/${filename}`,
-        publishedOn: new Date('12,1,2002'),
-      },
-    });
-    console.log("!!!!!   new painting created   !!!!!");
-    revalidatePath('/dashboard/paintings');
-    redirect('/dashboard/paintings');
+//   // check author first
+//   const authorRecord = await prisma.author.findFirst({
+//     where: {
+//       name: author,
+//     },
+//   });
 
-  } catch (error) {
-    console.log(error);
-    return {
-      message: "Database Error: Failed to Create Invoice.",
-    };
-  }
-}
+//   if (!authorRecord) {
+//     throw new Error(`No author found with name ${validatedData.data.author}`);
+//   }
+
+//   try {
+//     await writeFile(path.join(process.cwd(), "/public/uploads/" + filename), buffer);
+//     await prisma.book.create({
+//       data: {
+//         title: title,
+//         author: {
+//           connect: {
+//             id: authorRecord.id,
+//           },
+//         },
+//         //imgURL: `uploads/${filename}`,
+//         publishedOn: new Date('12,1,2002'),
+//       },
+//     });
+//     console.log("!!!!!   new painting created   !!!!!");
+//     revalidatePath('/dashboard/paintings');
+//     redirect('/dashboard/paintings');
+
+//   } catch (error) {
+//     console.log(error);
+//     return {
+//       message: "Database Error: Failed to Create Invoice.",
+//     };
+//   }
+// }
